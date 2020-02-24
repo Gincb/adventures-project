13cf95406f35b0b8b5e130ad1ca98b965f3bbe28
Gintare B <gintare.b@gmail.com>
2020-02-24 10:37:36 +0100

    fetch and test

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
 const link = "https://spreadsheets.google.com/feeds/list/1SQeili57IfygzYF7DD56KJAniwXYIeGCbWZdphcn0Ds/od6/public/values?alt=json";
     window.addEventListener("DOMContentLoaded", getData);
+
+    function getData() {
+        fetch(link)
+        .then(res=>res.json())
+        .then(handleData);
+    }
+
+    function handleData(data) {
+        const myData = data.feed.entry;
+        console.log(myData);
+        myData.forEach(showData);
+    }
+
+    function showData(singleRowData) {
+        console.log(singleRowData.gsx$location.$t, " - ", singleRowData.gsx$price.$t, ", ",singleRowData.gsx$name.$t); //test
+    }
