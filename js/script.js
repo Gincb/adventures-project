653bcc26a9c537cf1ec787a200a1b016be00be72
Gintare B <gintare.b@gmail.com>
2020-02-24 10:30:37 +0100

    added api

---
+++ js/script.js
@@ -0,0 +1,2 @@
+const link = "https://spreadsheets.google.com/feeds/list/1SQeili57IfygzYF7DD56KJAniwXYIeGCbWZdphcn0Ds/od6/public/values?alt=json";
+    window.addEventListener("DOMContentLoaded", getData);
