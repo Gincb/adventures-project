5049f74e391fb3c6c3e375041444d87fb39cc47e
Gintare B <gintare.b@gmail.com>
2020-02-25 11:06:56 +0100

    scroll-to-top button

--- js/script.js
+++ js/script.js
@@ -80,3 +80,19 @@ function showData(singleRowData) {
     place.appendChild(myClone);
 
 }
+
+scroll = document.getElementById("scroll");
+window.onscroll = function() { scrollDisplay() };
+
+function scrollDisplay() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scroll.style.display = "block";
+    } else {
+        scroll.style.display = "none";
+    }
+}
+
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
